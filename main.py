39ffb6e5dc6deab3d2283d8bcfd03bee0d1b85a0
john <Johnathan.Stephens.09@gmail.com>
2023-09-04 03:41:28 +0200

    implement basic functionality
    capable of opening a window on every screen and
    closing app on press of the Escape key

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 import pyglet
 
 
+def run_pyglet_shenanigans():
+    display = pyglet.canvas.get_display()
+    screens = display.get_screens()
+    windows = []
+
+    for screen in screens:
+        windows.append(pyglet.window.Window(fullscreen=True, screen=screen))
+
+    for window in windows:
+        @window.event
+        def on_key_press(symbol, modifiers):
+            if symbol == pyglet.window.key.ESCAPE:
+                pyglet.app.exit()
+
+    pyglet.app.run()
+
+
 def main():
-    pass
+    run_pyglet_shenanigans()
 
 
 if __name__ == "__main__":
