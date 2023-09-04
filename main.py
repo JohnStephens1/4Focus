370f2597515fe420a14cf9994e2da9ce50e0e4db
john <Johnathan.Stephens.09@gmail.com>
2023-09-04 02:44:59 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,41 +1,8 @@
-import tkinter as tk
-import screeninfo
-
-
-# todo
-#  window.lift() on click on any of the windows to all windows
-#  escape in any window closes program (all windows)
-
-
-
-def run_window():
-    window = tk.Tk()
-    # window.attributes('-fullscreen', True)
-    window.config(bg="black")
-
-    window.geometry(f"500x500+2000+200")
-    window.attributes("-fullscreen", True)
-
-    # new_window = tk.Toplevel()
-    # new_window.config(bg="black")
-
-    window.attributes()
-    window.mainloop()
-
-
-def get_total_screen_width():
-    return sum([monitor.width for monitor in screeninfo.get_monitors()])
-
-
-def run_screen_info():
-    # for monitor in screeninfo.get_monitors():
-    #     print(monitor.width)
-    pass
+import pyglet
 
 
 def main():
-    # run_window()
-    # run_screen_info()
+    pass
 
 
 if __name__ == "__main__":
