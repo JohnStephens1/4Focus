f111177d4cfaf47a0a1ce21d8807d7e97dd407b6
john <Johnathan.Stephens.09@gmail.com>
2023-09-04 04:05:02 +0200

    clean up code and add todo

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import pyglet
 
 
+# todo
+#  add title
+#  add sad cat meme as icon
+#
+# todo
+#  figure out how to reliably package program for non-python users
+
+
 def run_pyglet_shenanigans():
     display = pyglet.canvas.get_display()
     screens = display.get_screens()
@@ -10,12 +18,16 @@ def run_pyglet_shenanigans():
         windows.append(pyglet.window.Window(fullscreen=True, screen=screen))
 
     for window in windows:
-        @window.event
-        def on_key_press(symbol, modifiers):
-            if symbol == pyglet.window.key.ESCAPE:
-                pyglet.app.exit()
+        window_shenanigans(window)
 
     pyglet.app.run()
+
+
+def window_shenanigans(window):
+    @window.event
+    def on_key_press(symbol, modifiers):
+        if symbol == pyglet.window.key.ESCAPE:
+            pyglet.app.exit()
 
 
 def main():
