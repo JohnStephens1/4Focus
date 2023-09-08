ea6b29b796f0907340b9319916872472e3387a4b
john <Johnathan.Stephens.09@gmail.com>
2023-09-08 20:56:55 +0200

    add minimize and maximize hotkeys

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ import pyglet
 
 # todo
 #  add title
-#  add sad cat meme as icon
-#
+#  check for performance optimization
+
 # todo
 #  figure out how to reliably package program for non-python users
+#  add sad cat meme as icon
+#  write readme (4 everything ;-;)
 
 
-def run_pyglet_shenanigans():
+def run_app():
     display = pyglet.canvas.get_display()
     screens = display.get_screens()
     windows = []
@@ -18,20 +20,28 @@ def run_pyglet_shenanigans():
         windows.append(pyglet.window.Window(fullscreen=True, screen=screen))
 
     for window in windows:
-        window_shenanigans(window)
+        add_hotkeys(window, windows)
 
     pyglet.app.run()
 
 
-def window_shenanigans(window):
+def add_hotkeys(window, windows):
     @window.event
     def on_key_press(symbol, modifiers):
         if symbol == pyglet.window.key.ESCAPE:
             pyglet.app.exit()
 
+        if symbol == pyglet.window.key.M:
+            for win in windows:
+                win.minimize()
+
+        if symbol == pyglet.window.key.SPACE:
+            for win in windows:
+                win.maximize()
+
 
 def main():
-    run_pyglet_shenanigans()
+    run_app()
 
 
 if __name__ == "__main__":
